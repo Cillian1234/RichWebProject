f21be0b99ed9d8268fdd9fe9bb9e8a284a844c2d
Cillian <cillsm80@gmail.com>
2024-11-16 03:17:21 +0000

    Some work on login page

--- my-app/src/app/login/page.js
+++ my-app/src/app/login/page.js
@@ -3,16 +3,61 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Navbar from "@/components/Navbar";
+import {FormControl, Input, InputLabel, TextField} from "@mui/material";
+import Button from "@mui/material/Button";
 
 
 export default function MyApp() {
 
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            const res = await fetch('http://localhost:3000/api/login', {
+                method: 'POST',
+                body: JSON.stringify({
+                    username, password
+                })
+            })
+            if (!res.ok) throw new Error(res.statusText);
+
+            const {token} = await res.json();
+            document.cookie = `token= + ${token}; path=/`
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box>
             <Navbar />
 
-            <Box component="section" sx={{ p: 2, border: '1px dashed grey'}}>
-                This will be the login page.
+            <Box className="login">
+                <h1 className="loginHeader">Log in</h1>
+
+                <TextField
+                    variant="outlined"
+                    label="Username"
+                    required
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                />
+                <br/>
+                <TextField
+                    variant="outlined"
+                    label="Password"
+                    type="password"
+                    required
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
+
+                <Button variant="contained" className="loginButton">Log in</Button>
             </Box>
         </Box>
     );
